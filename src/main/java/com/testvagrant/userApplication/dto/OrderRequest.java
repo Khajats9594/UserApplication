18664e0c3f7b66abee69eb9ecddbbdde01b4879a
khajats <khajats@testvagrant.com>
2023-09-12 10:41:34 +0530

    request and response dto class is added

---
+++ src/main/java/com/testvagrant/userApplication/dto/OrderRequest.java
@@ -0,0 +1,16 @@
+package com.testvagrant.userApplication.dto;
+
+import com.testvagrant.userApplication.entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderRequest {
+
+    private Customer customer;
+}
